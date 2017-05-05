22180ee63f72e5043b49214d571cb7cb88c31918
耿小泰 <clawpo@foxmail.com>
2017-05-05 11:53:37 +0800

    精选首页的页面显示

--- app/src/main/java/cn/ucai/fulicenter/ui/activity/MainActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/ui/activity/MainActivity.java
@@ -1,28 +1,65 @@
 package cn.ucai.fulicenter.ui.activity;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.ui.fragment.BoutiqueFragment;
 import cn.ucai.fulicenter.ui.fragment.NewGoodsFragment;
 
 public class MainActivity extends AppCompatActivity {
+    NewGoodsFragment mNewGoodsFragment;
+    BoutiqueFragment mBoutiqueFragment;
+    Fragment[] mFragments;
+    int currentIndex,index;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initFragment();
         showFragment();
+    }
+
+    private void initFragment() {
+        mNewGoodsFragment = new NewGoodsFragment();
+        mBoutiqueFragment = new BoutiqueFragment();
+        mFragments = new Fragment[5];
+        mFragments[0] = mNewGoodsFragment;
+        mFragments[1] = mBoutiqueFragment;
     }
 
     private void showFragment() {
         getSupportFragmentManager().beginTransaction()
-                .add(R.id.fragment_container,new NewGoodsFragment())
+                .add(R.id.fragment_container,mNewGoodsFragment)
+                .add(R.id.fragment_container,mBoutiqueFragment)
+                .show(mNewGoodsFragment)
+                .hide(mBoutiqueFragment)
                 .commit();
     }
 
     public void onCheckedChange(View view){
+        switch (view.getId()){
+            case R.id.layout_new_good:
+                index = 0;
+                break;
+            case R.id.layout_boutique:
+                index = 1;
+                break;
+        }
+        setFragment();
+    }
+
+    private void setFragment() {
+        if (index!=currentIndex) {
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container,mFragments[index]);
+            fragmentTransaction.commit();
+            currentIndex = index;
+        }
     }
 
 
