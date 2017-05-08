84ef0dde2be2cbd131c7411cd418015d3649d6b3
耿小泰 <clawpo@foxmail.com>
2017-05-08 11:24:19 +0800

    商品详情显示数据

--- app/src/main/java/cn/ucai/fulicenter/ui/activity/GoodsDetailActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/ui/activity/GoodsDetailActivity.java
@@ -3,14 +3,24 @@ package cn.ucai.fulicenter.ui.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.application.I;
+import cn.ucai.fulicenter.data.bean.AlbumsBean;
 import cn.ucai.fulicenter.data.bean.GoodsDetailsBean;
+import cn.ucai.fulicenter.data.bean.PropertiesBean;
 import cn.ucai.fulicenter.data.net.GoodsModel;
 import cn.ucai.fulicenter.data.net.IGoodsModel;
 import cn.ucai.fulicenter.data.net.OnCompleteListener;
 import cn.ucai.fulicenter.data.utils.L;
+import cn.ucai.fulicenter.ui.view.FlowIndicator;
+import cn.ucai.fulicenter.ui.view.SlideAutoLoopView;
 
 /**
  * Created by clawpo on 2017/5/8.
@@ -20,19 +30,47 @@ public class GoodsDetailActivity extends AppCompatActivity {
     private static final String TAG = "GoodsDetailActivity";
     int goodsId;
     IGoodsModel model;
+    @BindView(R.id.tv_good_name_english)
+    TextView mTvGoodNameEnglish;
+    @BindView(R.id.tv_good_name)
+    TextView mTvGoodName;
+    @BindView(R.id.tv_good_price_shop)
+    TextView mTvGoodPriceShop;
+    @BindView(R.id.tv_good_price_current)
+    TextView mTvGoodPriceCurrent;
+    @BindView(R.id.salv)
+    SlideAutoLoopView mSalv;
+    @BindView(R.id.indicator)
+    FlowIndicator mIndicator;
+    @BindView(R.id.wv_good_brief)
+    WebView mWvGoodBrief;
+    Unbinder bind;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goods_detail);
-        goodsId = getIntent().getIntExtra(I.GoodsDetails.KEY_GOODS_ID,0);
-        L.e(TAG,"goodsId="+goodsId);
+        bind = ButterKnife.bind(this);
+        goodsId = getIntent().getIntExtra(I.GoodsDetails.KEY_GOODS_ID, 0);
+        L.e(TAG, "goodsId=" + goodsId);
         initData();
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (bind!=null){
+            bind.unbind();
+        }
+        if (mSalv!=null){
+            mSalv.stopPlayLoop();
+        }
+    }
+
     private void initData() {
-        if (goodsId==0){
+        if (goodsId == 0) {
             finish();
-        }else{
+        } else {
             model = new GoodsModel();
             loadData();
         }
@@ -43,9 +81,9 @@ public class GoodsDetailActivity extends AppCompatActivity {
                 new OnCompleteListener<GoodsDetailsBean>() {
                     @Override
                     public void onSuccess(GoodsDetailsBean result) {
-                        L.e(TAG,"result="+result);
-                        if (result!=null){
-
+                        L.e(TAG, "result=" + result);
+                        if (result != null) {
+                            showData(result);
                         }
                     }
 
@@ -54,5 +92,48 @@ public class GoodsDetailActivity extends AppCompatActivity {
 
                     }
                 });
+    }
+
+    private void showData(GoodsDetailsBean bean) {
+        mTvGoodNameEnglish.setText(bean.getGoodsEnglishName());
+        mTvGoodName.setText(bean.getGoodsName());
+        mTvGoodPriceCurrent.setText(bean.getCurrencyPrice());
+        mTvGoodPriceShop.setText(bean.getShopPrice());
+        mSalv.startPlayLoop(mIndicator,getAlbumImgUrl(bean),getAlbumImgCount(bean));
+        mWvGoodBrief.loadDataWithBaseURL(null,bean.getGoodsBrief(),I.TEXT_HTML,I.UTF_8,null);
+    }
+
+    private String[] getAlbumImgUrl(GoodsDetailsBean bean) {
+        AlbumsBean[] imgs = getAlbumImg(bean);
+        if (imgs!=null){
+            String[] urls = new String[imgs.length];
+            for (int i=0;i<imgs.length;i++) {
+                urls[i] = imgs[i].getImgUrl();
+            }
+            return urls;
+        }
+        return null;
+    }
+
+    private int getAlbumImgCount(GoodsDetailsBean bean) {
+        AlbumsBean[] imgs = getAlbumImg(bean);
+        if (imgs!=null){
+            return imgs.length;
+        }
+        return 0;
+    }
+
+    private AlbumsBean[] getAlbumImg(GoodsDetailsBean bean) {
+        if (bean.getProperties()!=null&&bean.getProperties().length>0){
+            PropertiesBean propertiesBean = bean.getProperties()[0];
+            if (propertiesBean!=null&&propertiesBean.getAlbums()!=null){
+                return propertiesBean.getAlbums();
+            }
+        }
+        return null;
+    }
+
+    @OnClick(R.id.backClickArea)
+    public void onClick() {
     }
 }
