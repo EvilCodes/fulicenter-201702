877d1e2f59d280159ef039004588605b0dbffd77
耿小泰 <clawpo@foxmail.com>
2017-05-11 11:29:10 +0800

    fragment和redioButton的统一

--- app/src/main/java/cn/ucai/fulicenter/ui/activity/MainActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/ui/activity/MainActivity.java
@@ -6,7 +6,10 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.RadioButton;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.application.FuLiCenterApplication;
 import cn.ucai.fulicenter.ui.fragment.BoutiqueFragment;
@@ -20,21 +23,43 @@ public class MainActivity extends AppCompatActivity {
     CategoryFragment mCategoryFragment;
     PersonalFragment mPersonalFragment;
     Fragment[] mFragments;
-    int currentIndex,index;
+    RadioButton[] mRadioButtons;
+    int currentIndex, index;
+    @BindView(R.id.layout_new_good)
+    RadioButton mLayoutNewGood;
+    @BindView(R.id.layout_boutique)
+    RadioButton mLayoutBoutique;
+    @BindView(R.id.layout_category)
+    RadioButton mLayoutCategory;
+    @BindView(R.id.layout_cart)
+    RadioButton mLayoutCart;
+    @BindView(R.id.layout_personal_center)
+    RadioButton mLayoutPersonalCenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         initFragment();
+        initRedioButton();
         showFragment();
+    }
+
+    private void initRedioButton() {
+        mRadioButtons = new RadioButton[5];
+        mRadioButtons[0] = mLayoutNewGood;
+        mRadioButtons[1] = mLayoutBoutique;
+        mRadioButtons[2] = mLayoutCategory;
+        mRadioButtons[3] = mLayoutCart;
+        mRadioButtons[4] = mLayoutPersonalCenter;
     }
 
     private void initFragment() {
         mNewGoodsFragment = new NewGoodsFragment();
         mBoutiqueFragment = new BoutiqueFragment();
         mCategoryFragment = new CategoryFragment();
-        mPersonalFragment= new PersonalFragment();
+        mPersonalFragment = new PersonalFragment();
         mFragments = new Fragment[5];
         mFragments[0] = mNewGoodsFragment;
         mFragments[1] = mBoutiqueFragment;
@@ -44,17 +69,17 @@ public class MainActivity extends AppCompatActivity {
 
     private void showFragment() {
         getSupportFragmentManager().beginTransaction()
-                .add(R.id.fragment_container,mFragments[0])
-                .add(R.id.fragment_container,mFragments[1])
-                .add(R.id.fragment_container,mFragments[2])
+                .add(R.id.fragment_container, mFragments[0])
+                .add(R.id.fragment_container, mFragments[1])
+                .add(R.id.fragment_container, mFragments[2])
                 .show(mFragments[0])
                 .hide(mFragments[1])
                 .hide(mFragments[2])
                 .commit();
     }
 
-    public void onCheckedChange(View view){
-        switch (view.getId()){
+    public void onCheckedChange(View view) {
+        switch (view.getId()) {
             case R.id.layout_new_good:
                 index = 0;
                 break;
@@ -65,9 +90,9 @@ public class MainActivity extends AppCompatActivity {
                 index = 2;
                 break;
             case R.id.layout_personal_center:
-                if (FuLiCenterApplication.getInstance().getCurrentUser()==null){
-                    startActivity(new Intent(MainActivity.this,LoginActivity.class));
-                }else {
+                if (FuLiCenterApplication.getInstance().getCurrentUser() == null) {
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                } else {
                     index = 4;
                 }
                 break;
@@ -76,17 +101,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setFragment() {
-        if (index!=currentIndex) {
+        if (index != currentIndex) {
             FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
             fragmentTransaction.hide(mFragments[currentIndex]);
-            if (!mFragments[index].isAdded()){
-                fragmentTransaction.add(R.id.fragment_container,mFragments[index]);
+            if (!mFragments[index].isAdded()) {
+                fragmentTransaction.add(R.id.fragment_container, mFragments[index]);
             }
             fragmentTransaction.show(mFragments[index]);
             fragmentTransaction.commit();
             currentIndex = index;
         }
+        setRedioButton();
     }
 
-
+    private void setRedioButton() {
+        for (int i = 0; i < mRadioButtons.length; i++) {
+            mRadioButtons[i].setChecked(i==index?true:false);
+        }
+    }
 }
