0b5c746ab5a89ee3339fd4312eddbde794d9b6e6
耿小泰 <clawpo@foxmail.com>
2017-05-18 11:30:47 +0800

    add sharesdk

--- app/src/main/java/cn/ucai/fulicenter/application/FuLiCenterApplication.java
+++ app/src/main/java/cn/ucai/fulicenter/application/FuLiCenterApplication.java
@@ -2,6 +2,7 @@ package cn.ucai.fulicenter.application;
 
 import android.app.Application;
 
+import cn.sharesdk.framework.ShareSDK;
 import cn.ucai.fulicenter.data.bean.User;
 
 /**
@@ -17,6 +18,7 @@ public class FuLiCenterApplication extends Application {
     public void onCreate() {
         super.onCreate();
         instance = this;
+        ShareSDK.initSDK(this);
     }
 
     public static FuLiCenterApplication getInstance() {
